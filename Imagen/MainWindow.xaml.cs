fad6e914aaa0e52e5eaf753306353cc2c4eef75a
AlexSinMas23 <ba.mota@iesdoctorbalmis.com>
2021-09-24 18:11:10 +0200

    Agregar archivos de proyecto.

---
+++ Imagen/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Imagen
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+        private void nivel1Boton_Click(object sender, RoutedEventArgs e) => this.foto.Opacity = 1;
+        private void nivel2Boton_Click(object sender, RoutedEventArgs e) => this.foto.Opacity = 0.66;
+        private void nivel3Boton_Click(object sender, RoutedEventArgs e) => this.foto.Opacity = 0.33;
+    }
+}
